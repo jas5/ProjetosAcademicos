ca8591ef0e5e9d92e99b200d888b735321498e16
PagContas <sistema@pagcontas.com.br>
2015-11-06 13:28:21 -0300

    commit

---
+++ projetoTesteSelenium/src/main/java/com/projetotesteselenium/NewClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projetotesteselenium;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author KillerBeeTwo
+ */
+public class NewClass {
+    
+    public static void main(String[] args){
+         BigDecimal num1 = BigDecimal.ZERO; 
+        BigDecimal num2 = BigDecimal.ONE;
+        BigDecimal num3 = num1.add(num2);
+        System.out.println(num3);
+    }
+    
+    
+}
